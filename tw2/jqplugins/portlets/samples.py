d5c48949d53107da39f7a92fa0204a1e54f8a6a7
Ralph Bean <rbean@redhat.com>
2012-05-20 16:49:28 -0400

    PEP8 in samples.py.

--- tw2/jqplugins/portlets/samples.py
+++ tw2/jqplugins/portlets/samples.py
@@ -12,34 +12,39 @@ from widgets import Column, ColumnLayout, Portlet
 
 
 class DemoLayout(ColumnLayout):
-    width='900px'
+    width = '900px'
 
     class col1(Column):
-        width='300px'
+        width = '300px'
+
         class portlet(Portlet):
             title = "Two widgets in a portlet"
             one_widget = twf.Label(text='Some content in a label widget')
             another_widget = twf.Label(text='Some other content in a label')
-        
+
         class whatever(Portlet):
             title = "One widget in a portlet"
             widgetry = twf.Label(text='Some content in a label widget')
 
     class col2(Column):
-        width='300px'
+        width = '300px'
+
         class portlet(Portlet):
             title = "Another portlet with only one widget"
             widgetry = twf.Label(text='Some content in a label widget')
 
     class col3(Column):
-        width='300px'
+        width = '300px'
+
         class portlet(Portlet):
             title = "A portlet with a whole form in it.. whoah."
+
             class Form(twf.TableForm):
                 title = twf.TextField(validator=twc.Required)
                 director = twf.TextField()
                 genre = twf.CheckBoxList(
                     options=['Action', 'Comedy', 'Romance', 'Sci-fi'])
+
                 class cast(twf.GridLayout):
                     extra_reps = 5
                     character = twf.TextField()
